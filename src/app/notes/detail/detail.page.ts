9d4585fb759cccb6b8f3dd3179355db074594884
Matt1479 <gitmatt1479@gmail.com>
2022-11-09 11:59:24 +0100

    feat(global): get note detail - by id

--- src/app/notes/detail/detail.page.ts
+++ src/app/notes/detail/detail.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { State } from 'src/app/store/app.reducer';
+import { Note } from '../note.model';
+import { selectNotes } from '../store/notes.selector';
 
 @Component({
   selector: 'app-detail',
@@ -7,22 +12,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
-  public title: string;
-  notes = [
-    {
-      title: 'Note Header',
-      date: new Date(),
-      thumbnail: '../../../assets/icon/favicon.png',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis, nisl vitae fringilla porttitor, sem eros vestibulum ligula, sit amet sodales ex lectus vel erat. Suspendisse auctor dapibus nunc, sed bibendum ipsum iaculis at. Suspendisse porta magna sit amet lorem efficitur, et pulvinar leo facilisis. Maecenas eget erat volutpat, pretium risus vitae, condimentum elit.',
-    },
-  ];
+  public noteId: string;
+  note: Note;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private store: Store<State>
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe((params) => {
-      this.title = params.noteId;
+    this.noteId = this.activatedRoute.snapshot.params.noteId;
+    this.store.pipe(select(selectNotes)).subscribe((n) => {
+      n.find((note, index) => {
+        if (n[index].id === this.noteId) {
+          this.note = n[index];
+        }
+        return n[index].id === this.noteId;
+      });
     });
+
+    // this.store.pipe(select(selectNotes)).subscribe((n) => console.log(n));
+    // this.notes$.subscribe((n) => {
+
+    //   console.log(this.note);
+    // });
   }
 }
