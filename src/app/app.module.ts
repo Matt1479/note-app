7ed3b5785af332e38c18a43f92f3448e6b37cae1
Matt1479 <gitmatt1479@gmail.com>
2022-11-09 11:55:12 +0100

    feat(global): Set up NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/app.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './store/app.effects';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([AppEffects]),
+    HttpClientModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { NotesPageRoutingModule } from './notes-routing.module';
 
 import { NotesPage } from './notes.page';
+import { NotesService } from './notes.service';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { notesFeatureKey, notesReducer } from './store/notes.reducer';
+import { NotesEffects } from './store/notes.effects';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    NotesPageRoutingModule
+    NotesPageRoutingModule,
+    StoreModule.forFeature(notesFeatureKey, notesReducer),
+    EffectsModule.forFeature([NotesEffects]),
   ],
-  declarations: [NotesPage]
+  declarations: [NotesPage],
+  providers: [NotesService],
 })
 export class NotesPageModule {}
